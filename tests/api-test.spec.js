6785d66c323158e10449f1dd0c43321c032349c5
Alvika Prahasta <alvikaajiandrianty@gmail.com>
2023-11-27 23:31:15 +0700

    fix automated testing

--- tests/api-test.spec.js
+++ tests/api-test.spec.js
@@ -1,316 +1,182 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
+test.describe.configure({ mode: "serial" });
 
-import axios from "axios";
+const resultRandom = Math.random().toString(36).substring(2, 7);
 
-const authCustomer = {
-  email: "alvikaajiandrianty@gmail.com",
-  password: "alvika123",
+const payloadCustomer = {
+  email: `${resultRandom}@example.com`,
+  password: `${resultRandom}`,
 };
 
-test("should be able to get wishlist by customer", async ({
-  page,
-  request,
-}) => {
-  const response = await request.post(
-    "http://localhost:9000/store/auth/token",
-    {
-      data: authCustomer,
-    }
-  );
-  console.log(await response.json());
-  expect(response.ok()).toBeTruthy();
-  expect(response.status()).toBe(200);
-  const responseBody = await response.json();
-  const token = responseBody.access_token;
-  console.log("New Token is: " + token);
+const authCustomer = {
+  email: payloadCustomer.email,
+  password: payloadCustomer.password,
+};
 
-  await page.setExtraHTTPHeaders({
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${token}`,
+const payloadAdmin = {
+  email: `admin@medusa-test.com`, // replace with the correct admin login data
+  password: `password`, // replace with the correct admin login data
+};
+
+let token;
+let tokenAdmin;
+let wishlist_name_id;
+let wishlist_id;
+
+test.use({
+  baseURL: "http://localhost:9000",
+  extraHTTPHeaders: {
+    Accept: "application/json",
+    Authorization: `token ${process.env.API_TOKEN}`,
+  },
+});
+
+test.beforeAll(async ({ request }) => {
+  // Register Customer
+  const responseCustomer = await request.post("/store/customers", {
+    data: payloadCustomer,
   });
+  expect(responseCustomer.ok()).toBeTruthy();
 
-  const responseWishlist = await page.goto(
-    `http://localhost:9000/store/wishlist/customer`
-  );
+  // Login Customer
+  const responseLogin = await request.post("/store/auth/token", {
+    data: authCustomer,
+  });
+  expect(responseLogin.ok()).toBeTruthy();
+  const loginResponseBody = await responseLogin.json();
+  token = loginResponseBody.access_token;
+
+  // Login Admin
+  const responseLoginAdmin = await request.post("/admin/auth/token", {
+    data: payloadAdmin,
+  });
+  expect(responseLoginAdmin.ok()).toBeTruthy();
+  const loginResponseAdminBody = await responseLoginAdmin.json();
+  tokenAdmin = loginResponseAdminBody.access_token;
+});
+
+test.beforeEach(async ({ request }) => {
+  const responseWishlist = await request.get(`/store/wishlist/customer`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
   expect(responseWishlist).toBeTruthy();
+  const wishlist = await responseWishlist.json();
+  wishlist_name_id = wishlist.wishlist[0]?.id;
 });
 
 test("should be able to create a wishlist", async ({ request }) => {
-  const loginResponse = await request.post(
-    "http://localhost:9000/store/auth/token",
-    {
-      data: authCustomer,
-    }
-  );
-
-  expect(loginResponse.ok()).toBeTruthy();
-  expect(loginResponse.status()).toBe(200);
-
-  const loginResponseBody = await loginResponse.json();
-  const token = loginResponseBody.access_token;
-
-  expect(token).toBeTruthy();
-
   const payload = {
     title: "play wright",
   };
 
-  const response = await axios.post(
-    `http://localhost:9000/store/wishlist`,
-    payload,
-    {
-      withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const response = await request.post(`/store/wishlist`, {
+    data: payload,
+    headers: { Authorization: `Bearer ${token}` },
+  });
 
-  if (response.status === 401) {
-    console.log("Unauthorized - Token might be invalid or expired.");
-  } else {
-    expect(response.status).toBe(201);
-    expect(response.data).toHaveProperty("message", "Success Insert Wishlist");
-  }
+  expect(response.status()).toBe(201);
+  expect(response).toBeTruthy();
 });
 
-test("should be able to delete a wishlist Name", async ({ request }) => {
-  const loginResponse = await request.post(
-    "http://localhost:9000/store/auth/token",
+test("should be able to get wishlist by customer", async ({ request }) => {
+  const responseWishlist = await request.get(`/store/wishlist/customer`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  expect(responseWishlist).toBeTruthy();
+  const wishlist = await responseWishlist.json();
+});
+
+test("should be able to create a wishlist item", async ({ request }) => {
+  const payload = {
+    variant_id: `variant_01HG004GJQR58TSZV2HPFW5SA7`,
+  };
+  const response = await request.post(
+    `/store/wishlist/${wishlist_name_id}/wishlist-item`,
     {
-      data: authCustomer,
+      data: payload,
+      headers: { Authorization: `Bearer ${token}` },
     }
   );
 
-  expect(loginResponse.ok()).toBeTruthy();
-  expect(loginResponse.status()).toBe(200);
-
-  const loginResponseBody = await loginResponse.json();
-  const token = loginResponseBody.access_token;
-
-  expect(token).toBeTruthy();
-
-  const wishlistNameId = "wishlistName_01HFXE1J2NB1M8AM0PQKPHYST1";
-
-  try {
-    const deleteRequest = await axios.delete(
-      `http://localhost:9000/store/wishlist/${wishlistNameId}`,
-      {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    expect(deleteRequest.status).toBe(200);
-    expect(deleteRequest.statusText).toBe("OK");
-    expect(deleteRequest.data).toHaveProperty("message", "Wishlist Deleted");
-  } catch (error) {
-    console.error("Error deleting wishlist:", error);
-    throw error;
-  }
+  expect(response.status()).toBe(201);
+  expect(response).toBeTruthy();
 });
 
 test("should be able to update a wishlist", async ({ request }) => {
-  const loginResponse = await request.post(
-    "http://localhost:9000/store/auth/token",
-    {
-      data: authCustomer,
-    }
-  );
-
-  expect(loginResponse.ok()).toBeTruthy();
-  expect(loginResponse.status()).toBe(200);
-
-  const loginResponseBody = await loginResponse.json();
-  const token = loginResponseBody.access_token;
-
-  expect(token).toBeTruthy();
-
-  const wishlistNameId = "wishlistName_01HFXE0SPSQ94TW3134W1J1XTE"; // Change This
   const data = {
     title: "Update By Playwright",
   };
 
-  try {
-    const updateRequest = await axios.put(
-      `http://localhost:9000/store/wishlist/${wishlistNameId}`,
+  const updateRequest = await request.put(
+    `/store/wishlist/${wishlist_name_id}`,
+    {
       data,
-      {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
 
-    expect(updateRequest.status).toBe(200);
-    expect(updateRequest.statusText).toBe("OK");
-    expect(updateRequest.data).toHaveProperty("message", "Wishlist Updated");
-  } catch (error) {
-    console.error("Error updating wishlist:", error);
-    throw error;
-  }
+  expect(updateRequest.status()).toBe(200);
+  expect(updateRequest).toBeTruthy();
 });
-
 test("should be able to delete a wishlist item", async ({ request }) => {
-  const loginResponse = await request.post(
-    "http://localhost:9000/store/auth/token",
-    {
-      data: authCustomer,
-    }
-  );
+  const responseWishlist = await request.get(`/store/wishlist/customer`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  expect(responseWishlist).toBeTruthy();
+  const wishlist = await responseWishlist.json();
+  wishlist_id = wishlist.wishlist[0]?.wishlists[0]?.id;
+  const id = wishlist_id;
+  const deleteRequest = await request.delete(`/store/wishlist-item/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 
-  expect(loginResponse.ok()).toBeTruthy();
-  expect(loginResponse.status()).toBe(200);
-
-  const loginResponseBody = await loginResponse.json();
-  const token = loginResponseBody.access_token;
-
-  expect(token).toBeTruthy();
-
-  const wishlistItemId = "wishlist_01HFXCCTBP1MPX7SXMHTAJZN9E"; // Change This
-
-  try {
-    const deleteRequest = await axios.delete(
-      `http://localhost:9000/store/wishlist-item/${wishlistItemId}`,
-      {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    expect(deleteRequest.status).toBe(200);
-    expect(deleteRequest.statusText).toBe("OK");
-    const responseBody = deleteRequest.data;
-    expect(responseBody).toHaveProperty("message", "Wishlist Item Deleted");
-  } catch (error) {
-    console.error("Error deleting wishlist item:", error);
-    throw error;
-  }
+  expect(deleteRequest.status()).toBe(200);
+  expect(deleteRequest).toBeTruthy();
 });
 
-test("should be able to create a wishlist item", async ({ request }) => {
-  const loginResponse = await request.post(
-    "http://localhost:9000/store/auth/token",
+test("should be able to delete a wishlist Name", async ({ request }) => {
+  const deleteRequest = await request.delete(
+    `/store/wishlist/${wishlist_name_id}`,
     {
-      data: authCustomer,
+      headers: { Authorization: `Bearer ${token}` },
     }
   );
-
-  expect(loginResponse.ok()).toBeTruthy();
-  expect(loginResponse.status()).toBe(200);
-
-  const loginResponseBody = await loginResponse.json();
-  const token = loginResponseBody.access_token;
-
-  expect(token).toBeTruthy();
-
-  const wishlistNameId = "wishlistName_01HFXE0SPSQ94TW3134W1J1XTE"; // Change This
-
-  const payload = {
-    variant_id: "variant_01HFRF924GBHHJ3B4XGGVTJG5F",
-  };
-
-  try {
-    const response = await axios.post(
-      `http://localhost:9000/store/wishlist/${wishlistNameId}/wishlist-item`,
-      payload,
-      {
-        withCredentials: true,
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    expect(response.status).toBe(201);
-    const responseBody = response.data;
-    expect(responseBody).toHaveProperty(
-      "message",
-      "Success Insert Wishlist Item"
-    );
-  } catch (error) {
-    console.error("Error creating wishlist item:", error);
-    throw error;
-  }
+  expect(deleteRequest.status()).toBe(200);
+  expect(deleteRequest).toBeTruthy();
 });
 
 test("should be able to get all wishlist and wishlist item by admin", async ({
-  page,
   request,
 }) => {
-  const response = await request.post(
-    "http://localhost:9000/admin/auth/token",
-    {
-      data: {
-        // change this
-        email: "admin@medusa-test.com",
-        password: "alvika123",
-      },
-    }
-  );
-  console.log(await response.json());
-  expect(response.ok()).toBeTruthy();
-  expect(response.status()).toBe(200);
-  const responseBody = await response.json();
-  const token = responseBody.access_token;
-  console.log("New Token is: " + token);
-
-  const customer_id = "cus_01HDVRJGHMVDAMWT73KKHSR9T2"; // change this
-
-  await page.setExtraHTTPHeaders({
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${token}`,
+  const customerlist = await request.get(`/admin/customers`, {
+    headers: { Authorization: `Bearer ${tokenAdmin}` },
+  });
+  const customer = await customerlist.json();
+  const customer_id = customer.customers[0]?.id;
+  const responseWishlist = await request.get(`/admin/${customer_id}/wishlist`, {
+    headers: { Authorization: `Bearer ${tokenAdmin}` },
   });
 
-  const responseWishlist = await page.goto(
-    `http://localhost:9000/admin/${customer_id}/wishlist`
-  );
-
+  expect(responseWishlist.status()).toBe(200);
   expect(responseWishlist).toBeTruthy();
 });
 
 test("should be able to get count product in wishlist customer", async ({
-  page,
   request,
 }) => {
-  const response = await request.post(
-    "http://localhost:9000/admin/auth/token",
+  const productlist = await request.get(`/admin/products`, {
+    headers: { Authorization: `Bearer ${tokenAdmin}` },
+  });
+  const product = await productlist.json();
+  const product_id = product.products[0]?.id;
+
+  const responseWishlist = await request.get(
+    `/admin/${product_id}/count/wishlist`,
     {
-      data: {
-        // change this
-        email: "admin@medusa-test.com",
-        password: "alvika123",
-      },
+      headers: { Authorization: `Bearer ${tokenAdmin}` },
     }
   );
-  console.log(await response.json());
-  expect(response.ok()).toBeTruthy();
-  expect(response.status()).toBe(200);
-  const responseBody = await response.json();
-  const token = responseBody.access_token;
-  console.log("New Token is: " + token);
-
-  const product_id = "prod_01HG004GH0CH737RYR70JTPSSF"; // change this
-
-  await page.setExtraHTTPHeaders({
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${token}`,
-  });
-
-  const responseWishlist = await page.goto(
-    `http://localhost:9000/admin/${product_id}/count/wishlist`
-  );
-
+  expect(responseWishlist.status()).toBe(200);
   expect(responseWishlist).toBeTruthy();
 });
